15b146e8c64f85c25d21662fb614fd8a77103111
sspatari <spatari.stanislav@gmail.com>
2017-05-15 21:15:32 +0300

    added floating point division solver program in lab5

--- lab#5/floating_point_division.asm
+++ lab#5/floating_point_division.asm
@@ -1,8 +1,15 @@
+;Floating point division Algorithm 1
+;With reeshtablishment of the remainder and shift left
+
 .data
-msg1 db 'Enter mx(4 hex): $'
+msg0 db 'Initial Data',10,13,'$'
+msg1 db 10, 13, 'Enter mx(4 hex): $'
 msg2 db 10, 13, 'Enter ex(2 hex): $'
 msg3 db 10, 13, 'Enter my(4 hex): $'
 msg4 db 10, 13, 'Enter ey(2 hex): $'
+msg5 db 10, 10, 13, 'Result', 10, 13, '$'
+msg6 db 10, 13, 'mz: $'
+msg7 db 10, 13, 'ez: $'
 
 mx dw ?
 ex db ?
@@ -11,26 +18,20 @@ ey db ?
 mz dw ?
 ez db ?
 
-buffer  db 10,?, 10 dup(0) 
+buffer  db ?,?, 5 dup(0) 
 
-.code
-main proc
-    mov ax, @data
-    mov ds, ax
-    
-    call inputInitData
-    
-    mov ah, 4Ch ; return control to OS
-    mov al, 00
+.code 
+
+inputInitData proc
+    mov dx, offset msg0 ;block displays msg0
+    mov ah, 09h
     int 21h
-main endp
- 
-
-inputInitData proc 
+    
     mov dx, offset msg1 ;block displays msg1
     mov ah, 09h
     int 21h
    
+    mov buffer[0], 5
     mov ax, 2 ; nr of bytes to represent the packed bcd written with hex
     push ax   ; passing parameters to proc
     lea di, mx
@@ -43,18 +44,20 @@ inputInitData proc
     mov ah, 09h
     int 21h
     
+    mov buffer[0], 3
     mov ax, 1 ; nr of bytes to represent the packed bcd written with hex
     push ax   ; passing parameters to proc
     lea di, ex
     push di   
     ; reset packed bcd:
-    mov word ptr [di], 0
+    mov byte ptr [di], 0
     call inputHexes ;will load packed bcd in ex
     
     mov dx, offset msg3 ;block displays msg3
     mov ah, 09h
     int 21h
-   
+    
+    mov buffer[0], 5
     mov ax, 2 ; nr of bytes to represent the packed bcd written with hex
     push ax   ; passing parameters to proc
     lea di, my
@@ -67,12 +70,13 @@ inputInitData proc
     mov ah, 09h
     int 21h
     
+    mov buffer[0], 3
     mov ax, 1 ; nr of bytes to represent the packed bcd written with hex
     push ax   ; passing parameters to proc
     lea di, ey
     push di   
     ; reset packed bcd:
-    mov word ptr [di], 0
+    mov byte ptr [di], 0
     call inputHexes ;will load packed bcd in ey
     
     ret
@@ -118,7 +122,10 @@ inputHexes proc ;parameters to push in stack ax,di before calling
 inputString:    
     lea dx, buffer ;input string
     mov ah, 0Ah
-    int 21h 
+    int 21h
+    
+    cmp buffer[1], 0 ;check if 0 char where introduced if yes, retype
+    je inputString 
      
     xor bx, bx
     mov bl, buffer[1] ;bx=nr of char inputed
@@ -145,7 +152,7 @@ packInput:
 	; to subtract 48 (30h) from its ascii code,
 	; to convert a char (A..F) to digit we need
 	; to subtract 55 (37h) from its ascii code,
-	; to convert a char (A..F) to digit we need
+	; to convert a char (a..f) to digit we need
 	; to subtract 87 (57h) from its ascii code,
 	; or just clear the upper nibble of a byte.
 	; mask:  00001111b  (0fh)
@@ -226,4 +233,152 @@ endTransform2:
     pop bp
     ret 4 ; nr of bytes to pop off the stack
 inputHexes endp
+
+
+floatingPointDivision proc
+    ;mx xor my,then push flag to stack
+    mov ax, mx
+    xor ax, my
+    pushf
+    
+    ;ez = ex - ey
+    mov ah, ex
+    sub ah, ey
+    mov ez, ah
+    
+    ;calc |mx|, |my|
+    mov ax, my
+    cwd ;if highest bit of ax=1 -> dx=0FFFFh else dx=0000h
+    xor ax, dx
+    sub ax, dx ; ax=|my|
+    mov bx, ax ; bx=|my| <-this is important
+    
+    mov ax, mx
+    cwd ;if highest bit of ax=1 -> dx=0FFFFh else dx=0000h
+    xor ax, dx
+    sub ax, dx ; ax=|mx|
+    
+    mov cx, 16 ;nr of bit in mz, needed for loop
+    xor mx, 0  ;mx=0
+    
+    ;|mx|-|my|
+    
+    
+findNextMantisaBit:
+    sub ax, bx ;rem = rem - |my|
+    js isNegative ;if rem<0
+    ;isPositive, rem>0
+    shl mz, 1  ;mz shift left
+    add mz, 1  ;mz = mz + 1
+    shl ax, 1  ;rem shift left
+    jmp label1
+    
+isNegative:
+    ;teoreticaly after shifting mz left we obtain a 0 at lowest bit
+    ;no need to add 0 to mz
+    shl mz, 1  ;mz shift left
+    add ax, bx ;rem = rem + |my|
+    shl ax, 1  ;rem shift left  
+
+label1:    
+    loop findNextMantisaBit
+              
+    ;now in mz is saved |mz|
+    ;check its sign
+    ;if 1 then mz = |mz|shifted right 1 time
+    ;if 0 then do nothing
+    cmp mz, 0 ;
+    jns checkSignFromStack
+    shr mz, 1
+    
+checkSignFromStack:    
+    ;restore flag, specificaly the sign flag
+    ;and modify corespondingly to sign
+    popf
+    jns endDivision
+    neg mz ; transforms to CC
+    inc ez
+endDivision:              
+    ret
+floatingPointDivision endp
+
+printHex proc ;dl as parameter
+              ;dl should contain the hex 
+    cmp dl, 09h
+    ja isLetter
+    add dl, 30h ;(0..9)
+    jmp endPrintHex
+isLetter:
+    add dl, 37h ;(A..F)
+endPrintHex:
+    mov ah, 02h ;printing the Hex
+    int 21h  
+    ret
+printHex endp
+
+
+printResult proc
+    mov dx, offset msg5 ;block displays msg5
+    mov ah, 09h
+    int 21h
+    
+    mov dx, offset msg6 ;block displays msg6
+    mov ah, 09h
+    int 21h
+    
+    mov bx, mz ;preparing to print mz
+    
+    mov dl, bh
+    shr dl, 4  ;shiting high 4 to low 4 bit
+    call printHex
+    
+    mov dl, bh
+    and dl, 0Fh ;use mask
+    call printHex
+    
+    mov dl, bl
+    shr dl, 4   ;shiting high 4 to low 4 bit 
+    call printHex
+    
+    mov dl, bl
+    and dl, 0Fh ;use mask
+    call printHex
+    
+    mov dx, offset msg7 ;block displays msg7
+    mov ah, 09h
+    int 21h
+    
+    mov bl, ez ;preparing to print mz
+    
+    mov dl, bl
+    shr dl, 4  ;shiting high 4 to low 4 bit
+    call printHex
+    
+    mov dl, bl
+    and dl, 0Fh ;use mask
+    call printHex
+
+    ret
+printResult endp         
+
+
+main proc
+    mov ax, @data
+    mov ds, ax
+    
+    call inputInitData; input mx,ex,my,ey
+    ;mov mx, 0B456h
+    ;mov my, 0A6AEh
+    ;mov ex, 33h
+    ;mov ey, 0DEh
+    
+    call floatingPointDivision ;calculate mz,ez
+    
+    call printResult ; prints mz and ez
+    
+    mov ah, 4Ch ; return control to OS
+    mov al, 00
+    int 21h
+main endp
+
 end main
